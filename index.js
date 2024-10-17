64082ad8e6bd81729b87cfcb1f90fda2f92d45b0
David Edward S <116884666+DavidTechie@users.noreply.github.com>
2024-10-17 18:02:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const apikey = "93df0c1027e56d627e4fa722423b3e13";
+const weatherDataEl = document.getElementById("weather-data");
+const cityInputEl = document.getElementById("city-input");
+const formEl = document.querySelector("form")
+
+formEl.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    const cityValue = cityInputEl.value;
+    getWeatherData(cityValue);
+});
+
+async function getWeatherData(cityValue) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`);
+       
+        if (response.ok) {
+            console.log("Ok");
+        } else {
+            console.error('Error fetching weather data:', response.statusText);
+        }
+        const data = await response.json();
+        const temperature = Math.round(data.main.temp);
+        const description = data.weather[0].description;
+        const icon = data.weather[0].icon;
+        const details = [
+            `Feels like:${Math.round(data.main.feels_like)}`,
+            `Humidity:${data.main.humidity}%` ,
+            `Wind speed:${data.wind.speed} m/s`
+        ];
+        weatherDataEl.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`;
+        weatherDataEl.querySelector(".temperature").textContent = `${temperature}°C`;
+        weatherDataEl.querySelector(".description").textContent = `${description}`;
+        weatherDataEl.querySelector(".details").innerHTML = details.map((detail)=>`<div>${detail}</div>`).join("");
+        
+    } catch (error) {
+        weatherDataEl.querySelector(".icon").innerHTML = "";
+        weatherDataEl.querySelector(".temperature").textContent = "";
+        weatherDataEl.querySelector(".description").textContent = "An error occured,please try again";
+        weatherDataEl.querySelector(".details").innerHTML ="";
+    }
+}
